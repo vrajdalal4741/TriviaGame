b5d505f14a4877743f231e76c1be1b7fe5fa9366
vrajdalal4741 <vrajdalal.popeyes@gmail.com>
2017-12-27 17:36:12 -0500

    updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,47 @@
 let i = 0;
+let rightAns = 0;
+let wrongns = 0;
 
 const Questions = [
-    ['Along with Jerry, which character does the co-creator of Seinfled play in the TV series?',
-        'George Constanza (Jason Alexander)',
-        'Elaine Benes (Julia Louis-Dreyfus)',
-        'Michael Richards (Cosmo Kramer)',
-        'George Steinbrenner (Larry David)'
-    ],
-    ['How many times have the Minneosta Vikings won the SuperBowl?',
-        'One',
-        'Three',
-        'Two',
-        'Zero'
-    ],
-    ['Bruce Lee was the founder of which style of Martial Arts?',
-        'Taekwondo',
+    {
+       question: 'Along with Jerry, which character does the co-creator of Seinfled play in the TV series?',
+       answers : [ 'George Constanza (Jason Alexander)',
+            'Elaine Benes (Julia Louis-Dreyfus)',
+            'Michael Richards (Cosmo Kramer)',
+            'George Steinbrenner (Larry David)'
+            ],
+        correct : 1,   
+    },
+    {
+        question:'How many times have the Minneosta Vikings won the SuperBowl?',
+        answers: ['One',
+                'Three',
+                'Two',
+                'Zero'
+                ],
+        correct : 3         
+    },
+    {
+        question: 'Bruce Lee was the founder of which style of Martial Arts?',
+        answers: ['Taekwondo',
         'Japanese Jujutsu',
         'Krav Maga',
         'Jeet Kune Do'
-    ],
-    ['In 2017, who did Food Beast rank as the best fried chicken chain in the U.S.?',
-        'KFC',
+        ],
+        correct : 2  
+    },
+    {
+        question: 'In 2017, who did Food Beast rank as the best fried chicken chain in the U.S.?',
+        answers :['KFC',
         'Chick-Fil-A',
         'Raising Canes',
-        'Popeye\'s'
-    ]
+        'Popeye\'s'],
+        correct : 1  
+    }
 ]
+
+console.log(Questions);
+
 let number = 16;
 
 let intervalId;
@@ -46,16 +62,57 @@ function decrement() {
     }
 }
 
+function stop(){
+    clearInterval(intervalId);
+}
+
 timer();
 
 function nextQuestion() {
-	$('#Question').html(Questions[i][0]);
-    $('#Aone').html(Questions[i][1]);
-    $('#Atwo').html(Questions[i][2]);
-    $('#Athree').html(Questions[i][3]);
-    $('#Afour').html(Questions[i][4]);
+	$('#Question').html(Questions[i].question);
+
+    for(let j = 0; j < 4; j++){
+        let answerDiv = $('<div>');
+        answerDiv.addClass('answer');
+        answerDiv.attr('data-id', j); //setting the attribute
+        answerDiv.html(Questions[i].answers[j]);
+        $('#Question').append(answerDiv);
+    }
+
+
+    $('.answer').click(function(){
+        let chosenAnswer = ($(this).attr('data-id')); //getting the attribute
+        let correctAnswer = Questions[i].correct;
+
+        console.log(chosenAnswer);
+        console.log(correctAnswer);
+
+        if(chosenAnswer === correctAnswer){
+            rightAns++;
+        }else{
+            wrongns++;
+        }
+
+        console.log("rightAns", rightAns);
+        console.log("wrongns", wrongns);
+
+        //chosenAnswer == correct answer
+        //correct++
+        //else
+        //wrong++
+
+        //stop timer, call next question again
+        stop();
+
+    });
+
     i++;
 }
+
+function showResults(){
+    //correct and wrong answers
+
+    //start game again
+}
+
 nextQuestion();
-let rightAns = 0;
-let wrongns = 0;
